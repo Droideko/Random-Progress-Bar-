d49734029a47cd064d6214fe738ee3a545eec1ad
Droideka <vindou@mail.ru>
2019-12-24 19:38:23 +0300

    fix multiply click on the button

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const btn = document.getElementById('continue');
 const progressBar = document.querySelector('.container-box__progress-bar');
 btn.addEventListener('click', e => {
+   btn.style.pointerEvents = 'none';
    let random = Math.floor(Math.random()*(100-10)+10);
    const computedStyle = getComputedStyle(progressBar);
-   let width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;   
+   let width = 0;   
    if (width) width = 0;
    let timer = setTimeout(f = () => {
       if (width < random){
@@ -11,6 +12,9 @@ btn.addEventListener('click', e => {
          progressBar.style.setProperty('--width', width);
          progressBar.dataset.label = `${Math.floor(width)}%`;
          timer = setTimeout(f, 10);
-      } else clearTimeout(timer);
+      } else {
+         clearTimeout(timer);
+         btn.style.pointerEvents = 'auto';
+      }
    },10);
 });
